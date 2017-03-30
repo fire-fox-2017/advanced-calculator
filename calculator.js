eb5f36128b1e5e51db5578bfe6fdb29c6207dcfc
Didit Aditya <email": null>
2017-03-30 18:45:45 +0700

    Update main.js

--- calculator.js
+++ calculator.js
@@ -48,12 +48,6 @@ class Calculator {
   }
 }
 
-let calc = new Calculator();
-calc.add(1).show();
-calc.clear().show();
-calc.add(1).substract(2).show().divide(3).square().circleArea().squareRoot().show();
-calc.clear().show();
-
 /** note : you can use several features from ecmascript, such as:
 * - Classes
 * - Default Parameters

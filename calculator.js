e0777e940fd429d1a0cd2b436dd9f630a37a6482
haryanawisnu <arya.wisnu05@gmail.com>
2017-03-31 11:00:18 +0700

    done

--- calculator.js
+++ calculator.js
@@ -2,19 +2,36 @@
 
 class Calculator {
   //write your code here
-  constructor () {
+  constructor (value) {
+    this.num = value;
   }
-  add () {
+  add (value) {
+    this.num += value;
+    return this.num;
   }
-  substract () {
+  substract (value) {
+    this.num -= value;
+    return this.num;
   }
-  multiply () {
+  multiply (value) {
+    this.num *= value;
+    return this.num;
   }
-  divide () {
+  divide (value) {
+    this.num /= value;
+    return this.num;
   }
-  square () {
+  square (value) {
+    this.num = Math.pow(this.num , value);
+    return this.num;
   }
-  squareRoot () {
+  squareRoot (value) {
+    this.num = Math.sqrt(this.num ,value);
+    return this.num;
+  }
+  Circle(){
+    this.num = 3.14 * (Math.pow(this.num , 2));
+    return this.num;
   }
 }
 
@@ -25,6 +42,8 @@ class Calculator {
 * - Template Literals
 * - Method Chaining
 */
+// let my_Calculator = new Calculator(4);
+// console.log(my_Calculator.square(2));
 
 module.exports = {
   Calculator

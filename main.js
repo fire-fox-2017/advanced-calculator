8b6a5d66d21867062b4163fc23c942b029b3b855
Ambo Dalle <ambodalle@Ambos-MacBook-Pro.local>
2017-04-03 14:37:49 +0700

    calculator

--- main.js
+++ main.js
@@ -3,3 +3,10 @@
 const {Calculator} = require("./calculator.js")
 
 // execute function on calculator.js in here
+
+let calc = new Calculator();
+calc.add(10).substract(2).print(); // 10-2 = 8
+calc.multiply(4).divide(2).print(); // 4 / 2 = 2
+calc.square(16).print();  // akar 16 = 4
+calc.getCircleArea(5).print(); 
+calc.squareRoot(5).print();
